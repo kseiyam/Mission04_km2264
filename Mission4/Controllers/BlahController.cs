72469f2b812f43c6f47c023e8e3a1d2762c024d4
Kitt Mori <km2264@byu.edu>
2023-02-06 19:19:13 -0700

    mission5

---
+++ Mission4/Controllers/BlahController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Mission4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controllers
+{
+    public class BlahController : Controller
+    {
+        public IActionResult Index()
+        {
+            //throw new IndexOutOfRangeException();
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GradeCalculator ()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GradeCalculator (GradeCalculatorModels model)
+        {
+            return View();
+        }
+    }
+}
---
+++ Mission4/Models/GradeCalculatorModels.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Models
+{
+    public class GradeCalculatorModels
+    {
+        [Required] //this makes sure that the assignments section has a value
+        [Range(0, 100.00)] //this makes sure that the inputted value is between 0 and 100.00
+        public float assignments { get; set; }
+        [Required]
+        [Range(0, 100.00)]
+        public float group_projects { get; set; }
+        [Required]
+        [Range(0, 100.00)]
+        public float quizzes { get; set; }
+        [Required]
+        [Range(0, 100.00)]
+        public float midterm { get; set; }
+        [Required]
+        [Range(0, 100.00)]
+        public float final { get; set; }
+        [Required]
+        [Range(0, 100.00)]
+        public float intex { get; set; }
+    }
+}
